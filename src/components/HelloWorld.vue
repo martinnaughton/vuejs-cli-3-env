9be715fe2d0205b9705f5450655209f640812ca5
martinnaughton <martin.oneachtain@gmail.com>
2019-03-29 18:21:32 +0000

    Added env variables

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -6,28 +6,12 @@
       check out the
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Vuejs Enviromental variables</h3>
     <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-mocha" target="_blank" rel="noopener">unit-mocha</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-e2e-nightwatch" target="_blank" rel="noopener">e2e-nightwatch</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
+      <li> The default env variable for deployment type: <b>{{ nodeEnv }}</b> </li>
+      <li> The variable will always show in any mode. It in the .env file: <b>{{ defaultEnv }}</b> </li>
+      <li> The variable will only show in development mode. It in the .env.development file:<b> {{ devEnv }}</b> </li>
+      <li> The variable will only show in production mode. It in the .env.production file:<b> {{ prodEnv }}</b> </li>
     </ul>
   </div>
 </template>
@@ -37,6 +21,22 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  computed: {
+    nodeEnv: function () {
+      return process.env.NODE_ENV
+    },
+    defaultEnv: function () {
+      return process.env.VUE_APP_TITLE
+    },
+    devEnv: function () {
+      return process.env.VUE_APP_DEV_TITLE
+    },
+    prodEnv: function () {
+      return process.env.VUE_APP_PROD_TITLE
+    }
+  },
+  mounted: function () {
   }
 }
 </script>
@@ -47,11 +47,9 @@ h3 {
   margin: 40px 0 0;
 }
 ul {
-  list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
 }
 a {
